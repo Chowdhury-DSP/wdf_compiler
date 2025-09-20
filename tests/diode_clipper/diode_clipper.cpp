868f39fa30bc789e49cf537abdefcc9bb762d393
jatin <jatinchowdhury18@gmail.com>
2025-09-19 22:11:08 -0400

    Starting on custom diode

---
+++ tests/diode_clipper/diode_clipper.cpp
@@ -0,0 +1,57 @@
+#include "rc_lowpass.h"
+
+#include "../chowdsp_wdf.h"
+#include <iostream>
+
+struct Reference_WDF
+{
+    chowdsp::wdft::ResistorT<float> R1 { 1000.0f };
+    chowdsp::wdft::CapacitorT<float> C1 { 1.0e-6f };
+    chowdsp::wdft::WDFSeriesT<float, decltype (R1), decltype (C1)> S1 { R1, C1 };
+    chowdsp::wdft::IdealVoltageSourceT<float, decltype (S1)> Vin { S1 };
+
+    void prepare (float fs)
+    {
+        C1.prepare (fs);
+    }
+
+    float process (float V)
+    {
+        Vin.setVoltage (V);
+        Vin.incident (S1.reflected());
+        S1.incident (Vin.reflected());
+        return chowdsp::wdft::voltage<float> (C1);
+    }
+};
+
+int main()
+{
+    std::cout << "RC Lowpass test\n";
+
+    static constexpr float fs = 48000.0f;
+
+    Reference_WDF ref {};
+    ref.prepare (fs);
+
+    Impedances impedances {};
+    calc_impedances (impedances, fs);
+    State state {};
+
+    float max_error = 0.0f;
+    for (int i = 0; i < 100; ++i)
+    {
+        const auto test_output = process (state, impedances, 1.0f);
+        const auto ref_output = ref.process (1.0f);
+        const auto error = std::abs (test_output - ref_output);
+        max_error = std::max (error, max_error);
+    }
+    std::cout << "Max Error: " << max_error << '\n';
+
+    if (max_error > 1.0e-3f)
+    {
+        std::cout << "Error is too large... failing test!\n";
+        return 1;
+    }
+
+    return 0;
+}
