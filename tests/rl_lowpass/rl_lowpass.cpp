943b8e33cb3906aecbdc52c19ef8999694e28ec9
jatin <jatinchowdhury18@gmail.com>
2025-10-07 12:32:05 -0400

    Fix namespace handling

--- tests/rl_lowpass/rl_lowpass.cpp
+++ tests/rl_lowpass/rl_lowpass.cpp
@@ -33,14 +33,14 @@ int main()
     Reference_WDF ref {};
     ref.prepare (fs);
 
-    rl_lowpass::Impedances impedances {};
-    rl_lowpass::calc_impedances (impedances, fs);
-    rl_lowpass::State state {};
+    test::rl_lowpass::Impedances impedances {};
+    test::rl_lowpass::calc_impedances (impedances, fs);
+    test::rl_lowpass::State state {};
 
     float max_error = 0.0f;
     for (int i = 0; i < 100; ++i)
     {
-        const auto test_output = rl_lowpass::process (state, impedances, 1.0f);
+        const auto test_output = test::rl_lowpass::process (state, impedances, 1.0f);
         const auto ref_output = ref.process (1.0f);
         const auto error = std::abs (test_output - ref_output);
         max_error = std::max (error, max_error);
