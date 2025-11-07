8e392e45689444668de6aa63a3d129fd67e7baa0
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2025-11-07 17:47:01 -0500

    Performance comparisons with cycle counting (#19)
    * Working perf counters on ARM Mac
    
    * Testing Apple event counters on Intel Mac
    
    * Event counter print formatting tweaks
    
    * More test tweaks
    
    * RC lowpass cycle analysis
    
    * Working benchmarks for other circuits
    
    * Rough but working perf counters on Windows
    
    * Fix ifdef

--- tests/diode_clipper/diode_clipper.cpp
+++ tests/diode_clipper/diode_clipper.cpp
@@ -5,7 +5,9 @@
 #include <iostream>
 #include <random>
 
-// clang diode_clipper.cpp --std=c++20 -lstdc++ -o diode_clipper.exe && ./diode_clipper.exe
+#include "../performance_counters/event_counter.h"
+
+// clang diode_clipper.cpp --std=c++20 -lstdc++ -I../../lib -o diode_clipper.exe && ./diode_clipper.exe
 
 struct Reference_WDF
 {
@@ -65,42 +67,56 @@ int main()
     }
 
 #if RUN_BENCH
-    static constexpr int N = 10'000'000;
+    static constexpr int M = 10'000'000;
+    static constexpr int n_iter = 10;
 
-    auto* data_in = (float*) malloc (N * sizeof (float));
-    auto* data_out = (float*) malloc (N * sizeof (float));
+    auto* data_in = (float*) malloc (M * sizeof (float));
+    auto* data_out = (float*) malloc (M * sizeof (float));
 
     std::random_device rd {};
     std::default_random_engine gen { rd() };
     std::uniform_real_distribution<float> dist { -1.0f, 1.0f };
-    for (int n = 0; n < N; ++n)
-        data_in[n] = dist (gen);
+    event_collector collector {};
 
     double ref_time, test_time;
     {
-        auto start = std::chrono::steady_clock::now();
+        event_aggregate aggregate {};
+        float save_out = 0.0f;
+        for (int iter = 0; iter < n_iter; ++iter)
+        {
+            for (int n = 0; n < M; ++n)
+                data_in[n] = dist (gen);
 
-        for (int n = 0; n < N; ++n)
-            data_out[n] = ref.process (data_in[n]);
+            collector.start();
+            for (int n = 0; n < M; ++n)
+                data_out[n] = ref.process (data_in[n]);
 
-        auto end = std::chrono::steady_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::duration<double, std::milli>> (end - start);
-        std::cout << data_out[N-1] << '\n';
-        std::cout << "chowdsp_wdf: " << duration.count() << " milliseconds" << std::endl;
-        ref_time = duration.count();
+            aggregate << collector.end();
+            save_out += data_out[M-1];
+        }
+        std::cout << save_out << '\n';
+        pretty_print (aggregate, M, "chowdsp_wdf");
+        ref_time = aggregate.elapsed_ns();
     }
 
     {
-        auto start = std::chrono::steady_clock::now();
+        event_aggregate aggregate {};
+        float save_out = 0.0f;
+        for (int iter = 0; iter < n_iter; ++iter)
+        {
+            for (int n = 0; n < M; ++n)
+                data_in[n] = dist (gen);
 
-        for (int n = 0; n < N; ++n)
-            data_out[n] = process (state, impedances, data_in[n]);
+            collector.start();
+            for (int n = 0; n < M; ++n)
+                data_out[n] = process (state, impedances, data_in[n]);
 
-        auto end = std::chrono::steady_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::duration<double, std::milli>> (end - start);
-        std::cout << data_out[N-1] << '\n';
-        std::cout << "wdf_compiler: " << duration.count() << " milliseconds" << std::endl;
-        test_time = duration.count();
+            aggregate << collector.end();
+            save_out += data_out[M-1];
+        }
+        std::cout << save_out << '\n';
+        pretty_print (aggregate, M, "wdf_compiler");
+        test_time = aggregate.elapsed_ns();
     }
     std::cout << "wdf_compiler is " << ref_time / test_time << "x faster\n";
 
--- tests/rc_lowpass/rc_lowpass.cpp
+++ tests/rc_lowpass/rc_lowpass.cpp
@@ -3,6 +3,69 @@
 #include "../chowdsp_wdf.h"
 #include <iostream>
 #include <fstream>
+#include <random>
+
+#include "../performance_counters/event_counter.h"
+
+// C1 reflected:
+//   - 1 load (C1 state)
+// R1 reflected: (no-op)
+// S1 reflected:
+//   - 1 add
+// Vin incident: (no-op)?
+// Vin reflected:
+//   - 1 fmsub
+// S1 incident:
+//   - 1 load
+//   - 1 fmsub
+//   - 2 add
+// C1 incident:
+//   - 1 add
+//   - 1 negate
+// C1 state update:
+//   - 1 store
+// Voltage:
+//   - 1 mult
+//   - 1 add
+//
+// TOTAL (Apple M1): Reference: https://dougallj.github.io/applecpu/firestorm.html
+//   - FMSUB    -> (4/0.25  cycles/instruction)
+//   - FADD     -> (2/0.25  cycles/instruction)
+//   - FNEG     -> (2/0.25  cycles/instruction)
+//   - LDR      -> (~2.5/0.333 cycles/instruction)
+//   - STR      -> (0.5/1   cycles/instruction)
+// Compiler output (ARM):
+// process(State&, Impedances const&, float):
+//   cycle (~2.5):
+//     movi    d1, #0000000000000000
+//     ldr     s2, [x0]
+//     fmov    s4, #2.00000000
+//   cycle (2):
+//     fadd    s3, s2, s1
+//   cycle (4):
+//     fmadd   s0, s0, s4, s3
+//   cycle (~2.5):
+//     ldr     s4, [x1]
+//     fadd    s3, s0, s1
+//   cycle (2):
+//     fadd    s3, s2, s3
+//   cycle (2):
+//     fmsub   s1, s4, s3, s1
+//   cycle (2):
+//     fadd    s1, s0, s1
+//   cycle (2):
+//     fsub    s0, s2, s1
+//     fmov    s2, #0.50000000
+//   cycle (2):
+//     fneg    s1, s1
+//     fmul    s0, s0, s2
+//   cycle (1):
+//     str     s1, [x0]
+//     ret
+// TOTAL: 22 cycles/sample
+// Measured:
+//   - chowdsp_wdf:  ~22.06 cycles/sample
+//   - wdf_compiler: ~22.04 cycles/sample
 
 struct Reference_WDF
 {
@@ -59,6 +122,65 @@ int main()
     std::ofstream ofp { "data.bin", std::ios::out | std::ios::binary };
     ofp.write(reinterpret_cast<const char*>(ref_output.data()), N * sizeof (float));
     ofp.close();
+
+#if RUN_BENCH
+    static constexpr int M = 100'000'000;
+    static constexpr int n_iter = 4;
+
+    auto* data_in = (float*) malloc (M * sizeof (float));
+    auto* data_out = (float*) malloc (M * sizeof (float));
+
+    std::random_device rd {};
+    std::default_random_engine gen { rd() };
+    std::uniform_real_distribution<float> dist { -1.0f, 1.0f };
+    event_collector collector {};
+
+    double ref_time, test_time;
+    {
+        event_aggregate aggregate {};
+        float save_out = 0.0f;
+        for (int iter = 0; iter < n_iter; ++iter)
+        {
+            for (int n = 0; n < M; ++n)
+                data_in[n] = dist (gen);
+
+            collector.start();
+            for (int n = 0; n < M; ++n)
+                data_out[n] = ref.process (data_in[n]);
+
+            aggregate << collector.end();
+            save_out += data_out[M-1];
+        }
+        std::cout << save_out << '\n';
+        pretty_print (aggregate, M, "chowdsp_wdf");
+        ref_time = aggregate.elapsed_ns();
+    }
+
+    {
+        event_aggregate aggregate {};
+        float save_out = 0.0f;
+        for (int iter = 0; iter < n_iter; ++iter)
+        {
+            for (int n = 0; n < M; ++n)
+                data_in[n] = dist (gen);
+
+            collector.start();
+
+            for (int n = 0; n < M; ++n)
+                data_out[n] = process (state, impedances, data_in[n]);
+
+            aggregate << collector.end();
+            save_out += data_out[M-1];
+        }
+        std::cout << save_out << '\n';
+        pretty_print (aggregate, M, "wdf_compiler");
+        test_time = aggregate.elapsed_ns();
+    }
+    std::cout << "wdf_compiler is " << ref_time / test_time << "x faster\n";
+
+    free (data_in);
+    free (data_out);
+#endif
 
     return 0;
 }
