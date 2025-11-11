332bdc3e46246d3eec47af754f22011add23208a
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2025-11-11 17:42:05 -0500

    Support for double and SIMD data types (#20)
    * Support for double-precision float
    
    * Working C++ test with SIMD
    
    * Fix half literal

--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -7,7 +7,7 @@ source ~/.bashrc
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sudo_pass=$(more ~/spass)
 fi
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 if [[ -n "$1" ]]; then
     cpp_compiler="$1"
@@ -15,13 +15,13 @@ else
     cpp_compiler="clang"
 fi
 
+if [ ! -d "${SCRIPT_DIR}/xsimd" ]; then
+    git clone https://github.com/xtensor-stack/xsimd "${SCRIPT_DIR}/xsimd"
+fi
 if [[ "$*" = *bench* ]]; then
     echo "Running bench tests..."
-    if [ ! -d "${SCRIPT_DIR}/xsimd" ]; then
-        git clone https://github.com/xtensor-stack/xsimd "${SCRIPT_DIR}/xsimd"
-    fi
     bench_flags="-DRUN_BENCH=1 -O3 -I${SCRIPT_DIR}/xsimd/include"
-    echo "$bench_flags"
+    echo "Extra flags: $bench_flags"
 else
     bench_flags=""
 fi
@@ -38,8 +38,18 @@ cpp_test () {
    test="$1"
    echo "Running CPP Test: $test"
    cd "${SCRIPT_DIR}/${test}"
-   $wdf_compiler "${test}.wdf" "${test}.h"
-   $cpp_compiler "${test}.cpp" ${bench_flags} -I../../lib --std=c++20 ${libcpp_flag} -o "${test}.exe"
+
+   wdf_compiler_flags=""
+   cpp_compiler_flags=${libcpp_flag}
+   if [[ "$test" == *"double"* ]]; then
+       wdf_compiler_flags="-dtype double"
+   elif [[ "$test" == *"simd"* ]]; then
+       wdf_compiler_flags="-dtype WDF_Float"
+       cpp_compiler_flags="${cpp_compiler_flags} -I${SCRIPT_DIR}/xsimd/include"
+   fi
+
+   $wdf_compiler "${test}.wdf" "${test}.h" ${wdf_compiler_flags}
+   $cpp_compiler "${test}.cpp" ${bench_flags} -I../../lib --std=c++20 ${cpp_compiler_flags} -o "${test}.exe"
    if [[ "$OSTYPE" == "darwin"* ]]; then
       echo ${sudo_pass} | sudo -S "./${test}.exe"
    else
@@ -51,7 +61,13 @@ jai_test () {
    test="$1"
    echo "Running JAI Test: $test"
    cd "${SCRIPT_DIR}/${test}"
-   $wdf_compiler -lang jai "${test}.wdf" "${test}.jai"
+
+   wdf_compiler_flags="-lang jai"
+   if [[ "$test" == *"double"* ]]; then
+       wdf_compiler_flags="${wdf_compiler_flags} -dtype float64"
+   fi
+
+   $wdf_compiler "${test}.wdf" "${test}.jai" ${wdf_compiler_flags}
    jai -quiet "${test}_test.jai"
    if [[ "$OSTYPE" == "win32" || "$OSTYPE" == "msys" ]]; then
       "./${test}_test.exe"
@@ -70,12 +86,16 @@ test () {
 
 if [[ "$*" = *bench* ]]; then
    cpp_test rc_lowpass
+   cpp_test rc_lowpass_double
+   cpp_test rc_lowpass_simd
    cpp_test preamp_eq_comb
    cpp_test diode_clipper
    cpp_test simple_triode
    cpp_test baxandall_eq
 else
    test rc_lowpass cpp jai
+   test rc_lowpass_double cpp jai
+   test rc_lowpass_simd cpp
    test rc_bandpass cpp jai
    test rl_lowpass cpp jai
    test rc_lowpass_var cpp jai
