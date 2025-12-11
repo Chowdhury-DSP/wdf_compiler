4ef6f1eac71bbbfda8ab6a6e716114a74a58e84f
jatin <jatinchowdhury18@gmail.com>
2025-12-11 07:09:25 -0800

    Working series resistor reduction

---
+++ tests/reductions_test/reductions_test.cpp
@@ -0,0 +1,131 @@
+#include "reductions_test.h"
+
+#include "../chowdsp_wdf.h"
+#include <iostream>
+#include <fstream>
+#include <random>
+
+#include "../performance_counters/event_counter.h"
+
+struct Reference_WDF
+{
+    chowdsp::wdft::ResistorT<float> R1 { 1.0e3f };
+    chowdsp::wdft::ResistorT<float> R2 { 5.0e3f };
+    chowdsp::wdft::WDFSeriesT<float, decltype (R1), decltype (R2)> S1 { R1, R2 };
+
+    chowdsp::wdft::ResistorT<float> R3 { 4.0e3f };
+    chowdsp::wdft::WDFSeriesT<float, decltype (S1), decltype (R3)> S2 { S1, R3 };
+
+    chowdsp::wdft::ResistorT<float> Rl { 100.0e3f };
+    chowdsp::wdft::WDFParallelT<float, decltype (S2), decltype (Rl)> P1 { S2, Rl };
+    chowdsp::wdft::IdealVoltageSourceT<float, decltype (P1)> Vin { P1 };
+
+    void prepare (float fs)
+    {
+    }
+
+    float process (float V)
+    {
+        Vin.setVoltage (V);
+        Vin.incident (P1.reflected());
+        P1.incident (Vin.reflected());
+        return chowdsp::wdft::voltage<float> (Rl);
+    }
+};
+
+int main()
+{
+    std::cout << "RC Lowpass test\n";
+
+    static constexpr float fs = 48000.0f;
+
+    Reference_WDF ref {};
+    ref.prepare (fs);
+
+    Impedances impedances {};
+    calc_impedances (impedances, fs, { .R2_value = ref.R2.wdf.R });
+    State state {};
+
+    static constexpr int N = 100;
+    std::array<float, N> ref_output {};
+    float max_error = 0.0f;
+    for (int i = 0; i < 100; ++i)
+    {
+        const auto test_output = process (state, impedances, 1.0f);
+        ref_output[i] = ref.process (1.0f);
+        const auto error = std::abs (test_output - ref_output[i]);
+        max_error = std::max (error, max_error);
+    }
+    std::cout << "Max Error: " << max_error << '\n';
+
+    if (max_error > 1.0e-3f)
+    {
+        std::cout << "Error is too large... failing test!\n";
+        return 1;
+    }
+
+    std::ofstream ofp { "data.bin", std::ios::out | std::ios::binary };
+    ofp.write(reinterpret_cast<const char*>(ref_output.data()), N * sizeof (float));
+    ofp.close();
+
+#if RUN_BENCH
+    static constexpr int M = 100'000'000;
+    static constexpr int n_iter = 4;
+
+    auto* data_in = (float*) malloc (M * sizeof (float));
+    auto* data_out = (float*) malloc (M * sizeof (float));
+
+    std::random_device rd {};
+    std::default_random_engine gen { rd() };
+    std::uniform_real_distribution<float> dist { -1.0f, 1.0f };
+    event_collector collector {};
+
+    double ref_time, test_time;
+    {
+        event_aggregate aggregate {};
+        float save_out = 0.0f;
+        for (int iter = 0; iter < n_iter; ++iter)
+        {
+            for (int n = 0; n < M; ++n)
+                data_in[n] = dist (gen);
+
+            collector.start();
+            for (int n = 0; n < M; ++n)
+                data_out[n] = ref.process (data_in[n]);
+
+            aggregate << collector.end();
+            save_out += data_out[M-1];
+        }
+        std::cout << save_out << '\n';
+        pretty_print (aggregate, M, "chowdsp_wdf");
+        ref_time = aggregate.elapsed_ns();
+    }
+
+    {
+        event_aggregate aggregate {};
+        float save_out = 0.0f;
+        for (int iter = 0; iter < n_iter; ++iter)
+        {
+            for (int n = 0; n < M; ++n)
+                data_in[n] = dist (gen);
+
+            collector.start();
+
+            for (int n = 0; n < M; ++n)
+                data_out[n] = process (state, impedances, data_in[n]);
+
+            aggregate << collector.end();
+            save_out += data_out[M-1];
+        }
+        std::cout << save_out << '\n';
+        pretty_print (aggregate, M, "wdf_compiler");
+        test_time = aggregate.elapsed_ns();
+    }
+    std::cout << "wdf_compiler is " << ref_time / test_time << "x faster\n";
+
+    free (data_in);
+    free (data_out);
+#endif
+
+    return 0;
+}
