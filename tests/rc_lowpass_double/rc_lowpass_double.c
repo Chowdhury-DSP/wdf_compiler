4f399f421af8d3131033a54ebe2da771409561ac
jatin <jatinchowdhury18@gmail.com>
2026-02-11 02:20:16 -0800

    RC lowpass tests are passing

--- tests/rc_lowpass_double/rc_lowpass_double.c
+++ tests/rc_lowpass_double/rc_lowpass_double.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "rc_lowpass_c.h"
+#include "rc_lowpass_double_c.h"
 
-static float* get_data(size_t* count)
+static double* get_data(size_t* count)
 {
     FILE* file = fopen ("data.bin", "rb");
     fseek (file, 0, SEEK_END);
@@ -16,18 +16,18 @@ static float* get_data(size_t* count)
     size_t bytes_read = fread (buffer, 1, file_size, file);
     assert (bytes_read == file_size);
 
-    *count = bytes_read / sizeof (float);
-    return (float*) buffer;
+    *count = bytes_read / sizeof (double);
+    return (double*) buffer;
 }
 
 int main()
 {
-    printf ("RC Lowpass test\n");
+    printf ("RC Lowpass test (double-precision)\n");
 
     size_t count;
-    float* ref_output = get_data (&count);
+    double* ref_output = get_data (&count);
 
-    const float fs = 48000.0f;
+    const float fs = 48000.0;
 
     struct Params params = {
         .R1_value = 1.0e+03,
@@ -37,17 +37,17 @@ int main()
     calc_impedances (&impedances, fs, params);
     struct State state = {};
 
-    float max_error = 0.0f;
+    double max_error = 0.0;
     for (size_t n = 0; n < count; ++n)
     {
-        float test_output = process (&state, &impedances, 1.0f);
-        float error = fabsf (test_output - ref_output[n]);
-        max_error = fmaxf (error, max_error);
+        double test_output = process (&state, &impedances, 1.0);
+        double error = fabs (test_output - ref_output[n]);
+        max_error = fmax (error, max_error);
     }
     printf ("Max error: %e\n", max_error);
     free (ref_output);
 
-    if (max_error > 1.0e-4f)
+    if (max_error > 1.0e-4)
     {
         printf("Error is too large... failing test!\n");
         return 1;
--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -58,6 +58,21 @@ cpp_test () {
    fi
 }
 
+c_test () {
+   test="$1"
+   echo "Running C Test: $test"
+   cd "${SCRIPT_DIR}/${test}"
+
+   wdf_compiler_flags="-lang c"
+   if [[ "$test" == *"double"* ]]; then
+       wdf_compiler_flags="${wdf_compiler_flags} -dtype double"
+   fi
+
+   $wdf_compiler "${test}.wdf" "${test}_c.h" ${wdf_compiler_flags}
+   clang ${test}.c -o ${test}_c.exe
+   "./${test}_c.exe"
+}
+
 jai_test () {
    test="$1"
    echo "Running JAI Test: $test"
@@ -82,6 +97,7 @@ test () {
    # The C++ test needs to run first,
    # since it generates the reference data.
    if [[ "$*" = *cpp* ]]; then cpp_test $test; fi
+   if [[ "$*" = *c_lang* ]]; then c_test $test; fi
    if [[ "$*" = *jai* ]]; then jai_test $test; fi
 }
 
@@ -99,27 +115,27 @@ if [[ "$*" = *bench* ]]; then
    cpp_test reductions_circuit2
    cpp_test analog_eq
 else
-   test rc_lowpass cpp jai
-   test rc_lowpass_double cpp jai
+   test rc_lowpass cpp c_lang jai
+   test rc_lowpass_double cpp c_lang jai
    test rc_lowpass_simd cpp
-   test rc_bandpass cpp jai
-   test rl_lowpass cpp jai
-   test rc_lowpass_var cpp jai
-   test rl_lowpass_var cpp jai
-   test rc_bandpass_var cpp jai
-   test rc_lowpass_2ins cpp jai
-   test preamp_eq cpp jai
-   test preamp_eq_comb cpp jai
-   test hard_clipper cpp jai
-   test diode_clipper cpp jai
-   test diode_circuit cpp jai
-   test simple_triode cpp jai
-   test bassman_tone_stack cpp jai
-   test baxandall_eq cpp jai
-   test pulse_shaper cpp jai
-   test reductions_circuit cpp jai
-   test reductions_circuit2 cpp jai
-   test analog_eq cpp jai
+   # test rc_bandpass cpp jai
+   # test rl_lowpass cpp jai
+   # test rc_lowpass_var cpp jai
+   # test rl_lowpass_var cpp jai
+   # test rc_bandpass_var cpp jai
+   # test rc_lowpass_2ins cpp jai
+   # test preamp_eq cpp jai
+   # test preamp_eq_comb cpp jai
+   # test hard_clipper cpp jai
+   # test diode_clipper cpp jai
+   # test diode_circuit cpp jai
+   # test simple_triode cpp jai
+   # test bassman_tone_stack cpp jai
+   # test baxandall_eq cpp jai
+   # test pulse_shaper cpp jai
+   # test reductions_circuit cpp jai
+   # test reductions_circuit2 cpp jai
+   # test analog_eq cpp jai
 fi
 
 if [[ "$*" = *bad_configs* ]]; then
