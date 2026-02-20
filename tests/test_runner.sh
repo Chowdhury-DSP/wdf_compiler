375157b71f7375e3fae7548c1b1639022b4376c7
jatinchowdhury18 <jatinchowdhury18@gmail.com>
2026-02-20 01:12:24 -0500

    CVs || R reduction (#34)

--- tests/test_runner.sh
+++ tests/test_runner.sh
@@ -131,27 +131,27 @@ if [[ "$*" = *bench* ]]; then
    cpp_test reductions_circuit2
    cpp_test analog_eq
 else
-   # test rc_lowpass cpp jai c_lang rust
-   # test rc_lowpass_double cpp jai c_lang rust
-   # test rc_lowpass_simd cpp
-   # test rc_bandpass cpp jai c_lang rust
-   # test rl_lowpass cpp jai c_lang rust
-   # test rc_lowpass_var cpp jai c_lang rust
-   # test rl_lowpass_var cpp jai c_lang rust
-   # test rc_bandpass_var cpp jai c_lang rust
-   # test rc_lowpass_2ins cpp jai c_lang rust
-   # test preamp_eq cpp jai c_lang rust
-   # test preamp_eq_comb cpp jai c_lang rust
-   # test hard_clipper cpp jai c_lang rust
-   # test diode_clipper cpp jai
-   # test diode_circuit cpp jai
-   # test simple_triode cpp jai
-   # test bassman_tone_stack cpp jai c_lang rust
-   # test baxandall_eq cpp jai c_lang rust
-   # test pulse_shaper cpp jai
-   # test reductions_circuit cpp jai c_lang rust
-   # test reductions_circuit2 cpp jai c_lang rust
-   # test analog_eq cpp jai rust
+   test rc_lowpass cpp jai c_lang rust
+   test rc_lowpass_double cpp jai c_lang rust
+   test rc_lowpass_simd cpp
+   test rc_bandpass cpp jai c_lang rust
+   test rl_lowpass cpp jai c_lang rust
+   test rc_lowpass_var cpp jai c_lang rust
+   test rl_lowpass_var cpp jai c_lang rust
+   test rc_bandpass_var cpp jai c_lang rust
+   test rc_lowpass_2ins cpp jai c_lang rust
+   test preamp_eq cpp jai c_lang rust
+   test preamp_eq_comb cpp jai c_lang rust
+   test hard_clipper cpp jai c_lang rust
+   test diode_clipper cpp jai
+   test diode_circuit cpp jai
+   test simple_triode cpp jai
+   test bassman_tone_stack cpp jai c_lang rust
+   test baxandall_eq cpp jai c_lang rust
+   test pulse_shaper cpp jai
+   test reductions_circuit cpp jai c_lang rust
+   test reductions_circuit2 cpp jai c_lang rust
+   test analog_eq cpp jai rust
    test channel_hpf cpp jai c_lang rust
 fi
 
